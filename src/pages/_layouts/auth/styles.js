24737110585e7d37614f9c1661df4352ab8c65da
PedroDiniz <pedro_nork@hotmail.com>
2019-09-05 17:54:32 -0300

    fixes

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -4,6 +4,7 @@ import { darken } from 'polished';
 // alinha ao centro
 export const Wrapper = styled.div`
   height: 100%;
+  min-height: 100vh;
   background: linear-gradient(-90deg, #7159c1, #ab59c1);
   display: flex;
   justify-content: center;
